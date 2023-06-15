16d4b4a9506e9f540f007f410e7e72b41f257e57
Corina <corina@danitt.com>
2023-06-15 19:41:35 +1000

    basic routing - done

--- src/server.js
+++ src/server.js
@@ -5,11 +5,14 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (request, response) => {
-	response.json({
-		message:"Welcome to the note taking backend"
-	});
+  response.json({
+    message: "Welcome to the note taking backend"
+  });
 });
 
+const notesRouter = require('./routes/notes_routes')
+app.use("/notes", notesRouter)
+
 module.exports = {
-	app
+  app
 }
