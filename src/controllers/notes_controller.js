9ccd93e01738b02822314f5936f5d5f94d780584
Corina <corina@danitt.com>
2023-06-15 21:00:16 +1000

    last part of class

--- src/controllers/notes_controller.js
+++ src/controllers/notes_controller.js
@@ -1,21 +1,29 @@
+const Note = require('../models/notes')
 
-const getNotes = (request, response) => {
-  response.json(
-    { "message": "The list of notes goes here" }
-  )
+const getNotes = async (request, response) => {
+
+  let notes = await Note.find()
+  response.send(notes)
 }
 
-const createNote = (request, response) => {
+const createNote = async (request, response) => {
+  let newNote = new Note({
+    title: request.body.title,
+    description: request.body.description,
+    isCompleted: false,
+    dueDate: new Date().setDate(new Date().getDate() + 1),
+    createdAtDate: Date.now()
+  })
+  await newNote.save()
+  response.status(201)
+  response.json({ note: newNote })
+}
+
+const deleteAllNotes = async (request, response) => {
+  await Note.deleteMany({})
   response.json({
-    "note": {
-      "id": 1,
-      "title": request.body.title,
-      "description": request.body.description,
-      "isCompleted": false,
-      "dueDate": new Date().setDate(new Date().getDate() + 1),
-      "createdAtDate": Date.now()
-    }
+    "message": "All notes deleted"
   })
 }
 
-module.exports = { getNotes, createNote }
+module.exports = { getNotes, createNote, deleteAllNotes }
