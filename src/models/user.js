fa56e12835de3b4a75bc4d26bea9264de2d56a73
Corina <corina@danitt.com>
2023-06-20 20:04:24 +1000

    catch up

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  notes: [{ type: mongoose.Types.ObjectId, ref: 'Note' }]
+})
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User
+
+/*
+option1 
+user{
+    username, 
+    password, 
+    email
+}
+notes{
+    title, 
+    description, 
+    ...
+    username/user_id
+}
+
+option2 
+user{
+    username, 
+    password, 
+    email
+    notes = [note_id, note_id, note_id]
+}
+notes{
+    title, 
+    description, 
+    ...
+
+}
+
+*/
--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001
 const HOST = process.env.HOST || '127.0.0.1'
 
 const helmet = require('helmet')
-app.use(Helmet());
+app.use(helmet());
 app.use(helmet.permittedCrossDomainPolicies());
 app.use(helmet.referrerPolicy());
 app.use(helmet.contentSecurityPolicy({
@@ -29,16 +29,42 @@ app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-async function dbConnect() {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/note_taking_db')
-    console.log("Database connected!")
-  } catch (error) {
-    console.log(`dbConnect failed, error: ${JSON.stringify(error)}`)
-  }
+let databaseURL = "";
+switch (process.env.NODE_ENV.toLowerCase()) {
+  case "production":
+    databaseURL = process.env.DATABASE_URL;
+    break;
+  case "development":
+    databaseURL = 'mongodb://localhost:27017/note_taking_db';
+    break;
+  case "test":
+    databaseURL = 'mongodb://localhost:27017/note_taking_db_test';
+    break;
+  default:
+    console.error("Wrong environment mode, database cannot connect");
 }
 
-dbConnect()
+const { databaseConnector } = require("./database")
+databaseConnector(databaseURL).then(() => {
+  console.log("connected to the db!")
+}).catch(error => {
+  console.log("could not connect to the db!")
+  console.log(error)
+})
+
+app.get("/databaseHealth", (request, response) => {
+  let databaseState = mongoose.connection.readyState;
+  let databaseName = mongoose.connection.name;
+  let databaseModels = mongoose.connection.modelNames();
+  let databaseHost = mongoose.connection.host;
+
+  response.json({
+    readyState: databaseState,
+    dbName: databaseName,
+    dbModels: databaseModels,
+    dbHost: databaseHost
+  })
+});
 
 app.get("/", (request, response) => {
   response.json({
@@ -48,6 +74,9 @@ app.get("/", (request, response) => {
 
 const notesRouter = require('./routes/notes_routes')
 app.use("/notes", notesRouter)
+
+const usersRouter = require('./routes/users_routes')
+app.use("/users", usersRouter)
 
 app.get('*', (request, response) => {
   response.status(404)
